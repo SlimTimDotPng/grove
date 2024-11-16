f72d7340fa6527fabb86f6e825fd7ae4b6752231
ik746cd <ivan.kuslita@student.tuke.sk>
2024-11-16 18:49:13 +0100

    Fixed Merge Conflicts

--- src/trie/index.js
+++ src/trie/index.js
@@ -1,2 +1,4 @@
 export {default as Trie} from "./Trie";
 export {default as TrieNode} from "./TrieNode.js";
+export {default as CardinalTrie} from "./CardinalTrie.js";
+export {default as CardinalTrieNode} from "./CardinalTrieNode.js";
